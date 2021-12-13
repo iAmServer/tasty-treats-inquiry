f46fabda9e7426532330542c6544a6cf8391badd
Ola Dayo <dasther@outlook.com>
2021-12-13 15:23:38 +0100

    Technical Test

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Dependencies: express, body-parser, mongoose, cors, dotenv
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
@@ -22,7 +23,8 @@ if (process.env.STORAGE_TYPE === "DB") {
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(`${__dirname}/dist/`));
+app.use(express.static(path.join(__dirname, "dist")));
+// app.use(express.static(`${__dirname}/dist/`));
 app.use("/api/contact", contactRoutes);
 
 // Server port listening
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,13 +14,13 @@ const routes: Routes = [
     component: FormComponent,
   },
   {
-    path: 'inquires/list',
+    path: 'inquiries/list',
     component: ListComponent,
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
